274160b4c2d8abf0ccb2a058908004251990b77a
Devyesh Tandon <devyeshtandon@gmail.com>
2015-11-03 15:57:24 +0530

    A-8

---
+++ a8-120010008/kernalDefinitions.py
@@ -0,0 +1,44 @@
+from numpy import pi, sqrt, exp
+
+def Gaussian1D(h, q):
+	temp = 1.0/sqrt(pi)/h*exp(-q**2)
+	return temp
+
+def CubicSpline1D(h, q):
+	q = abs(q)
+	q = q/h;
+	if q<1.0:
+		temp = 2.0/3/h*(1.0-(3.0/2.0*(q**2)*(1.0-q/2)))
+	elif q<2.0:
+		temp = 2.0/12/h*(2-q)**3
+	else:
+		temp = 0.0
+
+	return temp	
+
+def DerCubicSpline1D(h, q):
+
+	if q>0:
+		sign = 1
+	else:
+		sign = -1
+	q = q/h
+	q = abs(q)
+        if q<1.0:
+                temp = (-2.0*q/h + 1.5*q**2/h)/h
+        elif q<2.0:
+                temp = -1.0/2.0/(h**2)*(2-q)**2
+        else:
+                temp = 0
+
+        return temp*sign 
+
+def DerGaussian1D(h,q):
+	if q>0:
+		sign = 1
+	else:
+		sign = -1
+
+	q = abs(q)
+	temp = -2.0*q/(h**2)/sqrt(pi)*exp(-q**2)
+	return temp*sign
---
+++ a8-120010008/main.py
@@ -0,0 +1,67 @@
+from ps import initialize, propogate
+from numpy import arange, zeros
+import matplotlib.pyplot as plt
+from realSol import exactSol
+
+def setup():
+	[Pa, h] = initialize();
+	dt = 0.001
+	time = arange(0, 0.2, dt)
+
+	for t in range(len(time)):
+#		print t
+		[Pa, h] = propogate(Pa, h, dt)
+		
+	analyticalSolution = exactSol(arange(-0.5,0.5,0.0025))
+#	plotGraph(Pa, analyticalSolution)
+
+def plotGraph(Pa, AS):
+        x = zeros(400)
+        y = zeros(400)
+        w = zeros(400)
+        z = zeros(400)
+        a = zeros(400)
+
+        xa = zeros(400)
+        ya = zeros(400)
+        wa = zeros(400)
+        za = zeros(400)
+        aa = zeros(400)
+
+
+        for i in range(400, 800):
+                y[i-400] = Pa[i].p
+		w[i-400] = Pa[i].rho
+		z[i-400] = Pa[i].u
+		a[i-400] = Pa[i].e
+                x[i-400] = Pa[i].x
+	
+	xa = arange(-0.5,0.5, 0.0025)
+	ya = AS[2]
+	wa = AS[0]
+	za = AS[1]
+	aa = AS[3]
+
+
+	f, ax = plt.subplots(2, 2)
+	ax[0,0].plot(x,a)
+	ax[0,0].plot(xa,aa)
+	ax[0,0].set_title('Energy Profile')
+
+	ax[0,1].plot(x,w)
+	ax[0,1].plot(xa, wa)
+	ax[0,1].set_title('Density Profile')
+
+	ax[1,0].plot(x,y)
+	ax[1,0].plot(xa, ya)
+	ax[1,0].set_title('Pressure Profile')
+
+	ax[1,1].plot(x,z)
+	ax[1,1].plot(xa, za)
+	ax[1,1].set_title('Velocity Profile')
+
+        plt.show()
+
+
+if __name__ == '__main__':
+	setup();
